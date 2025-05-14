2965e28a5517fc906948fe56fb1d8889f504d875
Guymaster <guymaster25@gmail.com>
2025-05-14 16:44:37 +0000

    feat: Auth system and views

---
+++ resources/views/home.blade.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    @include('partials.global-style')
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+    <title>Accueil</title>
+</head>
+<body>
+    @include('partials.header')
+    <main>
+        ACCUEIL ICI
+        <section class="container">
+            EXEMPLE DE CONTENU
+        </section>
+    </main>
+    @include('partials.footer')
+    <style>
+        CSS ICI
+    </style>
+</body>
+</html>
---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,10 @@
+<header>
+    LE HEADER
+</header>
+
+<style>
+    header {
+        background-color: var(--primary-color);
+        color: var(--background-color);
+    }
+</style>
