75d0d433f3aab04849b335172ab4926457ec96c9
Cypher1305 <k.yao1305@gmail.com>
2025-05-26 08:38:10 +0000

    front/login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -2,7 +2,7 @@
     <x-auth-card>
         <x-slot name="logo">
             <a href="/">
-                <x-application-logo class="w-20 h-20 fill-current text-gray-500" />
+                <img src="{{ asset('logo.jpg') }}" alt="Logo" class="w-20 h-20 mx-auto rounded-full shadow-lg border-4 border-white">
             </a>
         </x-slot>
 
@@ -54,3 +54,4 @@
         </form>
     </x-auth-card>
 </x-guest-layout>
+@include('partials.footer')
