5a550cb196a296811c1516935a692d944bc3c958
Cypher1305 <k.yao1305@gmail.com>
2025-05-27 07:21:26 +0000

    fr

--- resources/views/auth/verify-email.blade.php
+++ resources/views/auth/verify-email.blade.php
@@ -7,12 +7,12 @@
         </x-slot>
 
         <div class="mb-4 text-sm text-gray-600">
-            {{ __('Thanks for signing up! Before getting started, could you verify your email address by clicking on the link we just emailed to you? If you didn\'t receive the email, we will gladly send you another.') }}
+            {{ __('Merci pour votre inscription ! Avant de commencer, cliquez sur le lien de vérification que nous vous avons envoyé. Pas reçu ? Nous pouvons vous en renvoyer un.') }}
         </div>
 
         @if (session('status') == 'verification-link-sent')
             <div class="mb-4 font-medium text-sm text-green-600">
-                {{ __('A new verification link has been sent to the email address you provided during registration.') }}
+                {{ __('Nous avons renvoyé un lien de vérification à l’e-mail que vous avez utilisé pour vous inscrire.') }}
             </div>
         @endif
 
@@ -22,7 +22,7 @@
 
                 <div>
                     <x-button>
-                        {{ __('Resend Verification Email') }}
+                        {{ __('Renvoyer l’email de vérification') }}
                     </x-button>
                 </div>
             </form>
