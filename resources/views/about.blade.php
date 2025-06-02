7d4a10c99e51ff981c7b24156d6d7de3d300df33
Cypher1305 <k.yao1305@gmail.com>
2025-06-02 12:22:13 +0000

    icon

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -17,13 +17,13 @@
                 HISTORIQUE
             </h2>
             <div class="fontb text-justify">
-                <p>
-                    Historiquement, l’Eglise Méthodiste est née du réveil de quatre (4) jeunes gens au plus fort de la décadence des mœurs en Angleterre, au 18e Siècle.<br>
-                    En Côte d’Ivoire, ce renouveau spirituel est marqué par le passage de <strong>Williams Wade HARRIS en 1914</strong>. L’Eglise protestante méthodiste connaitra plus tard des épiphénomènes au travers des chorales dans la région du sud. Dans les années 80, lors des différents camps bibliques Accepcistes qui avaient comme aumônier <strong>le Pasteur Benjamin BONI</strong>, ce réveil se fera sentir au travers des charismes (1 Corinthiens12). A la suite donc, on passera progressivement des camps bibliques à quelques églises de l’ancien circuit d’Abidjan-Nord (Yopougon, Adjamé, Cocody, Abobo, Attécoubé). <br>
-                    Des églises telles que 220 Logements, avec des doyens comme Eugène KOUADIO Jacob Koutouan ; Yopougon Horeb avec MELESS Bernardin seront les églises phares. C’est avec <strong>le Pasteur feu FATTOH</strong> qu’on assistera à la formation d’un groupe de prière à <strong>Adjamé-Ebenezer</strong>. <br>
-                    Le besoin et la soif des protestants méthodistes favoriseront la formation de plusieurs groupes, échappant au contrôle de l’Eglise. On parlera de mouvements charismatiques. Ces mouvements mal compris seront combattus. On dira par exemple « Nouveau Dieu », « ça ce n’est pas méthodiste ». Loin d’un découragement, ce mouvement va connaitre une ascension fulgurante, attirera plusieurs frères des autres communautés et suscitera plusieurs vocations en dehors de l’Eglise dans les années 90. <br>
+                <p class="p-3 leading-relaxed mb-4">
+                    Historiquement, l’Eglise Méthodiste est née du réveil de quatre (4) jeunes gens au plus fort de la décadence des mœurs en Angleterre, au 18e Siècle.<br><br>
+                    En Côte d’Ivoire, ce renouveau spirituel est marqué par le passage de <strong>Williams Wade HARRIS en 1914</strong>. L’Eglise protestante méthodiste connaitra plus tard des épiphénomènes au travers des chorales dans la région du sud. Dans les années 80, lors des différents camps bibliques Accepcistes qui avaient comme aumônier <strong>le Pasteur Benjamin BONI</strong>, ce réveil se fera sentir au travers des charismes (1 Corinthiens12). A la suite donc, on passera progressivement des camps bibliques à quelques églises de l’ancien circuit d’Abidjan-Nord (Yopougon, Adjamé, Cocody, Abobo, Attécoubé). <br><br>
+                    Des églises telles que 220 Logements, avec des doyens comme Eugène KOUADIO Jacob Koutouan ; Yopougon Horeb avec MELESS Bernardin seront les églises phares. C’est avec <strong>le Pasteur feu FATTOH</strong> qu’on assistera à la formation d’un groupe de prière à <strong>Adjamé-Ebenezer</strong>. <br><br>
+                    Le besoin et la soif des protestants méthodistes favoriseront la formation de plusieurs groupes, échappant au contrôle de l’Eglise. On parlera de mouvements charismatiques. Ces mouvements mal compris seront combattus. On dira par exemple « Nouveau Dieu », « ça ce n’est pas méthodiste ». Loin d’un découragement, ce mouvement va connaitre une ascension fulgurante, attirera plusieurs frères des autres communautés et suscitera plusieurs vocations en dehors de l’Eglise dans les années 90. <br><br>
                     Face à la nécessité et à la réalité, le corps pastoral se réunira à Bouaké en l’an 2000 et jettera les bases officielles du mouvement charismatique. On parlera alors de réveil méthodiste.<br>
-                    Plusieurs pasteurs se sont succédé, <strong>les Révérends DIARRAH, NDE Moïse, Isaac AKA, GBATTE Koudou et dépuis Octobre 2013 , l'Aumonier est le Rev Pasteur BECKEN KOKOBE</strong>.
+                    Plusieurs pasteurs se sont succédé, <strong>les Révérends DIARRAH, NDE Moïse, Isaac AKA, GBATTE Koudou et dépuis Octobre 2013, l'Aumonier est le Rev Pasteur BECKEN KOKOBE</strong>.
                 </p>
             </div>
         </section>
@@ -33,6 +33,7 @@
                 <p class="fontb">Pour une meilleure exécution du cahier de charges, le Mouvement de Réveil au plan National se dote des structures suivantes :</p><br>
                 <div class="flex flex-wrap justify-center gap-4">
                     <div class="card sermon-card h-100 shadow-sm p-2 bg-white rounded-lg transition-transform transform hover:scale-105 hover:shadow-lg">
+                        <i class="bx bx-desk"></i> 
                         <p class="p-1">Le secrétariat</p>
                     </div>
                     <div class="card sermon-card h-100 shadow-sm p-2 bg-white rounded-lg transition-transform transform hover:scale-105 hover:shadow-lg">
